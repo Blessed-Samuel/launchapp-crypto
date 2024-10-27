892128fc74a031b153d9557eeff1ee305df346ca
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-10-27 18:00:16 +0100

    Changed text in features.

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -3,7 +3,7 @@ import Container from "@/components/Container";
 export default function Features() {
     return (
         <Container>
-            <h1>Career Opportunities at Coinary <span className="text-red-200">Crypto</span></h1>
+            <h1> Features at Coinary <span className="text-red-200">Crypto</span></h1>
         </Container>
     );
 }
