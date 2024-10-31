213a4c77fc183efbe4b3fabdd8e84484da7fa080
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-10-31 15:49:48 +0100

    Modified default home page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,22 @@
-import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://coinary.crypto.com/"),
-  title: "coinary-crypto - crypto | Defi | NFTs | blockchain",
-  description:
-    "Ready to energize your life and turn it into earnings? With Move to Earn, your steps hold real value. Simply walk, run, or exercise to earn. Each step matters.",
-  keywords: [
-    "Cryptocurrency",
-    "Blockchain",
-    "Crypto Portfolio",
-    "DeFi",
-    "Ethereum",
-    "Bitcoin",
-    "Crypto Trading",
-    "Crypto Wallet",
-    "Smart Contracts",
-    "NFTs",
-    "Decentralized Finance",
-    "Web3",
-    "Crypto Mining",
-    "Cryptography",
-    "Crypto Security",
-  ],
-  openGraph: {
-    title: "coinary - crypto - crypto | Defi | NFTs | blockchain",
-    description:
-      "Ready to energize your life and turn it into earnings? With Move to Earn, your steps hold real value. Simply walk, run, or exercise to earn. Each step matters.",
-  },
-  alternates: {
-    canonical: "https://coinary.crypto.com/",
-  },
-};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly <{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body
         className={`${inter.className} bg-[#111] bg-[url('/Crypto-Background.svg')] bg-no-repeat w-full h-full bg-cover overflow-y-scroll overflow-x-hidden`}
       >
         <Navbar />
-        {children}
+        <main className='mt-16'>{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,41 @@
 import React from "react";
-import HomeRoot from "@/app/home/page";
+import Banner from "@/components/Banner";
+import Earn from "@/components/Earn";
 import Container from "@/components/Container";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://coinary.crypto.com/"),
+  title: 'Home | LaunchApp-Crypto',
+  description: 'With Move to Earn, your steps hold real value. Simply walk, run, or exercise to earn.',
+  keywords: [
+    "Cryptocurrency",
+    "Blockchain",
+    "Crypto Portfolio",
+    "DeFi",
+    "Ethereum",
+    "Bitcoin",
+    "Crypto Trading",
+    "Crypto Wallet",
+    "Smart Contracts",
+    "NFTs",
+    "Decentralized Finance",
+    "Web3",
+    "Crypto Mining",
+    "Cryptography",
+    "Crypto Security",
+  ],
+}
 
 export default function Home() {
   return (
-    <Container>
-      <main>
-        <div className="flex flex-col">
-          <HomeRoot />
-        </div>
-      </main>
-    </Container>
+    <>
+      <section>
+        <Container>
+          <Banner />
+          <Earn />
+        </Container>
+      </section>
+    </>
   );
 }
