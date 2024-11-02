4d460f50a94550cdae28937d15c1659b0f38d2a4
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-11-02 15:16:55 +0100

    Changed Components Imports In Earn Component.

--- components/Earn.tsx
+++ components/Earn.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Button from "./Button";
-import Card from "./Card";
+import Button from "@/components/Button";
+import Card from "@/components/Card";
 
 const Earn: React.FC<{}> = () => {
     return (
